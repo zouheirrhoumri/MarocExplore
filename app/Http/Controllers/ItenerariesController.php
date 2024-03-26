d4894cfbb712a85b87442e69dcdbff1920d3af19
zouheirrhoumri <zouheir.rhoumri@gmail.com>
2024-03-26 15:39:43 +0000

    strating the crud

---
+++ app/Http/Controllers/ItenerariesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Iteneraries;
+use App\Http\Requests\StoreItenerariesRequest;
+use App\Http\Requests\UpdateItenerariesRequest;
+
+class ItenerariesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreItenerariesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Iteneraries $iteneraries)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Iteneraries $iteneraries)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateItenerariesRequest $request, Iteneraries $iteneraries)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Iteneraries $iteneraries)
+    {
+        //
+    }
+}
---
+++ database/seeders/ItenerariesSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ItenerariesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
